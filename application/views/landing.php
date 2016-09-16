1e50388e2e745b2d9b95aff150b74e234f409a3f
fedorareis <fedorareis@gmail.com>
2016-09-16 01:23:59 -0700

    started support page. Only have abotu half of the content up

--- application/views/landing.php
+++ application/views/landing.php
@@ -6,51 +6,47 @@ $this->load->helper('url');
     <head>
         <title>Cru Central Coast</title>
         <link rel="icon" type="image/png" href="../../../assets/img/logo2.png">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-        <!-- Bootstrap -->
-        <!-- Page Formatting -->
         <link href="../assets/css/landing.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <img src="../assets/img/landing/cru-central-coast-resize.png" id="CCC"><img>
+        <img src="../assets/img/landing/cru-central-coast-resize.png" id="CCC">
 
         <p class="tagline">A family of ministries giving every student and faculty on California's Central Coast the opportunity to hear the Good News of Jesus Christ</p>
         <div class="holder">
             <a href="http://slo.crucentralcoast.com">
-                <img src="../assets/img/landing/slo-cru.png" class="ministry"><img>
+                <img src="../assets/img/landing/slo-cru.png" class="ministry">
             </a>
             <a href="http://slo.crucentralcoast.com">
-                <img src="../assets/img/landing/cuesta-cru.png" class="ministry"><img>
+                <img src="../assets/img/landing/cuesta-cru.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/groups/epicslo/">
-                <img src="../assets/img/landing/epic.png" class="ministry"><img>
+                <img src="../assets/img/landing/epic.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/groups/86209282438/">
-                <img src="../assets/img/landing/destino.png" class="ministry"><img>
+                <img src="../assets/img/landing/destino.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/groups/121310101276733/">
-                <img src="../assets/img/landing/greek-row.png" class="ministry"><img>
+                <img src="../assets/img/landing/greek-row.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/calpolyaia">
-                <img src="../assets/img/landing/athletes.png" class="ministry"><img>
+                <img src="../assets/img/landing/athletes.png" class="ministry">
             </a>
             <a href="http://facultycommonsslo.com/">
-                <img src="../assets/img/landing/faculty-commons.png" class="ministry"><img>
+                <img src="../assets/img/landing/faculty-commons.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/brandedchristiansinagriculture">
-                <img src="../assets/img/landing/branded.png" class="ministry"><img>
+                <img src="../assets/img/landing/branded.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/groups/385306844974162/">
-                <img src="../assets/img/landing/cuesta-north.png" class="ministry"><img>
+                <img src="../assets/img/landing/cuesta-north.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/CRUatHancock">
-                <img src="../assets/img/landing/hancock.png" class="ministry"><img>
+                <img src="../assets/img/landing/hancock.png" class="ministry">
             </a>
             <a href="https://www.facebook.com/groups/229636323901336/">
-                <img src="../assets/img/landing/cru-high.png" class="ministry"><img>
+                <img src="../assets/img/landing/cru-high.png" class="ministry">
             </a>
-            <img src="../assets/img/landing/choose.png" class="ministry" id="choose"><img>
+            <img src="../assets/img/landing/choose.png" class="ministry" id="choose">
         </div>
     </body>
 </html>
---
+++ application/views/support.php
@@ -0,0 +1,106 @@
+<?php
+$this->load->helper('html');
+$this->load->helper('url');
+?>
+<html>
+    <head>
+        <title>Cru Central Coast</title>
+        <link rel="icon" type="image/png" href="../../../assets/img/logo2.png">
+        <link href="../assets/css/landing.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <!--<img src="../assets/css/img/"> -->
+
+        <h5>App Support</h5>
+        <ol>
+            <li class="topic-header">General Use</li>
+            <ul>
+                <li class="sub-topic-header">Initial App Launch: </li>
+                <div class="sub-topic" id="initial-content">
+                    <p>
+                        On the launch of the Cru Central Coast App for the first time, you will be prompted to agree to our Terms of Service and to allow or deny access to push notifications and location services. The Terms of Service can be found below by following the Terms link at the bottom of the page. Cru Central Coast uses your location, if allowed, to make the RideSharing section of the App easy to use. Cru Central Coast uses push notifications to make users aware of changes to events or other information related only to the ministries the user is signed up for.
+                    </p>
+                    <p>
+                        After agreeing to the Terms of Service and allowing or denying push notifications and location services, you will be prompted to select which campuses and ministries you want to be a part of. The focuses of Cru Central Coast’s ministries are different and are outlined below. Each ministry is open to any and all students and faculty to attend. You may sign up for more than one ministry, but must sign up for one in order to continue in the App.
+                    </p>
+                    <p>
+                        <span class="underline">At Cal Poly</span>
+                        <br>
+                        Cru Cal Poly SLO: focus on the entire Cal Poly community
+                        <br>
+                        Branded: focus on Agriculture students and faculty
+                        <br>
+                        Destino: focus on Latino students and faculty
+                        <br>
+                        Epic: focus on Asian-American students and faculty
+                        <br>
+                        Faculty Commons: focus on professors and faculty
+                        <br>
+                        Fellowship of Christian Athletes in Action (FCAIA): focus on collegiate and club athletes
+                        <br>
+                        Greek Row: focus on students involved in fraternities and sororities
+                        <br>
+                        Valor: focus on students involved in ROTC
+                    </p>
+                    <p>
+                        Cru Cuesta: focus on the entire Cuesta community
+                    </p>
+                    <p>
+                        Cru Allan Hancock: focus on the entire Allan Hancock community in Santa Maria
+                    </p>
+                    <p>
+                        Cru Cuesta North: focus on the entire Cuesta North community in Paso Robles
+                    </p>
+                    <p>
+                        Cru SLO High: focus on the entire SLO High School community
+                    </p>
+                </div>
+                <li class="sub-topic-header">Home Page: </li>
+                <div class="sub-topic" id="initial-content">
+                    <p>
+                        The Home Page of the Cru Central Coast App contains information about upcoming rides and events corresponding to what the user signed up for. Links to the Cru
+                    </p>
+                </div>
+                <li class="sub-topic-header">Get Involved: </li>
+                <div class="sub-topic" id="initial-content">
+                    <p>
+                        This tab is divided into two pieces, the section on Community Groups and that of Ministry Teams. Community Groups are groups of students and faculty getting together to get to know one another more deeply and talk about spiritual topics. Ministry Teams offer ways to help out with events and become active in helping Cru Central Coast move forward.
+                    </p>
+                    <p>
+                        To sign up for a Community Group, tap Join a Community Group and choose the Ministry you would like to have that Community Group through. Enter your general information to help us narrow down what you are looking for and then click Submit to see the Community Groups that we offer. By clicking Sign Up, you become involved in that Community Group and the Community Group Leaders are sent your name and phone number to help connect you to the group.
+                    </p>
+                    <p>
+                        You can leave the Community Group at any time by going back to the Get Involved tab and tapping Leave.
+                    </p>
+                    <p>
+                        You can also join a Ministry Team by going to the Ministry Teams section at the top of the Get Involved tab. You will see brief descriptions of each team along with which ministry it serves. Tap Sign Up to get involved. Fill out your name and phone number and a notification will be sent to the leaders letting them know of your desire to join them.
+                    </p>
+                    <p>
+                        You can leave the Ministry Team at any time by going back to the Get Involved tab, tapping Ministry Teams, tapping the Ministry Team you want to leave, and the tapping Leave Team.
+                    </p>
+                </div>
+                <li class="sub-topic-header">Summer Missions: </li>
+                <div class="sub-topic" id="initial-content">
+                    <p>
+                        By tapping Summer Missions on the Navigation Bar, you will see all of the exciting opportunities that Cru offers for students and faculty to participate in over the summer. Each Summer Mission has general information displayed with more detail being shown if you tap. You can Learn More by tapping and seeing a website with more information.
+                    </p>
+                </div>
+                <li class="sub-topic-header">Settings: </li>
+                <div class="sub-topic" id="initial-content">
+                    <p>
+                        Tapping Settings on the Navigation Bar brings you to preferences that you can change. This includes the Campus(es) and Ministry(s) that you initially selected as well as the place to Login for additional resources. Changing the Campus and Ministry settings will impact what you see on other screens, particularly with Events and RideSharing.
+                    </p>
+                </div>
+                <li class="sub-topic-header"></li>
+                <div class="sub-topic" id="initial-content"></div>
+            </ul>
+            <li class="topic-header">First-time Drivers</li>
+            <div class="topic">
+                <p>
+                    Drivers who are not leaders are required to sign-up in person with a Cru Central Coast staff member, student leader, or faculty leader. This is to prevent drivers unassociated with Cru Central Coast from picking up students and faculty. Please see one of the people in those roles to sign-up to be a driver. Once you have gone through this process once, you don’t have to do it again.
+                </p>
+            </div>
+
+        </ol>
+    </body>
+</html>
